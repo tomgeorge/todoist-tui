306bece0e50b92ad1bc7357b16e13eefaa232e9b
Tom George <tg82490@gmail.com>
2023-09-12 15:38:53 -0700

    Add a project color thing because I was bored
    Signed-off-by: Tom George <tg82490@gmail.com>

--- main.go
+++ main.go
@@ -24,6 +24,31 @@ const (
 	TaskWindow   View = 2
 )
 
+var (
+  colors map[string]lipgloss.Color = map[string]lipgloss.Color {
+    "berry_red": lipgloss.Color("#b8256f"),
+    "red": lipgloss.Color("#db4035"),
+    "orange": lipgloss.Color("#ff9933"),
+    "yellow": lipgloss.Color("#fad000"),
+    "olive_green": lipgloss.Color("#afb83b"),
+    "lime_green": lipgloss.Color("#7ecc49"),
+    "green": lipgloss.Color("#299438"),
+    "mint_green": lipgloss.Color("#6accbc"),
+    "teal": lipgloss.Color("#158fad"),
+    "sky_blue": lipgloss.Color("#14aaf5"),
+    "light_blue": lipgloss.Color("#96c3eb"),
+    "blue": lipgloss.Color("#4073ff"),
+    "grape": lipgloss.Color("#884dff"),
+    "violet": lipgloss.Color("#af38eb"),
+    "lavender": lipgloss.Color("#eb96eb"),
+    "magenta": lipgloss.Color("#e05194"),
+    "salmon": lipgloss.Color("#ff8d85"),
+    "charcoal": lipgloss.Color("#808080"),
+    "grey": lipgloss.Color("#b8b8b8"),
+    "taupe": lipgloss.Color("#ccac93"),
+  }
+)
+
 var docStyle = lipgloss.NewStyle().Margin(1, 2)
 
 type model struct {
@@ -177,14 +202,15 @@ func (m model) View() string {
 			if m.projectCursor == i {
 				cursor = ">"
 			}
-			body += fmt.Sprintf("%s [%s]\n", cursor, project.Name)
+      projectColor := projectColor(project)
+			body += fmt.Sprintf("%s %s %s\n", cursor, projectColor, project.Name)
 		}
 		body += fmt.Sprintf("Selected Project: %s", m.selectedProject.Name)
 		body += "\nPress q to quit"
 		return body
 	case TasksView:
 		body := fmt.Sprintf("Tasks for %s", m.selectedProject.Name)
-		body += fmt.Sprintf("\n\nTasks For Project %s\n\n", m.selectedProject.Name)
+		body += fmt.Sprintf("\n\nTasks For Project %s %s\n\n", projectColor(m.selectedProject), m.selectedProject.Name)
 		if len(m.taskList.Items()) == 0 {
 			body += "No tasks found ✨"
 		}
@@ -194,6 +220,10 @@ func (m model) View() string {
 		return RenderTask(m.selectedTask)
 	}
 	return "Loading"
+}
+
+func projectColor(project types.Project) string {
+  return lipgloss.NewStyle().Background(colors[project.Color]).Render("  ")
 }
 
 func RenderTask(task types.Task) string {
